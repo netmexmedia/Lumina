e22a0e2f0ba478dec42a7073dfd7c8e5494efb37
Danny <d.verhoeven@vosensetz.nl>
2026-01-07 17:26:02 +0100

    Add DeleteDirective for handling delete operations in GraphQL queries

--- src/Directives/Definition/DeleteDirective.php
+++ src/Directives/Definition/DeleteDirective.php
@@ -9,44 +9,47 @@ use Netmex\Lumina\Contracts\ArgumentBuilderDirectiveInterface;
 use Netmex\Lumina\Contracts\FieldResolverInterface;
 use Netmex\Lumina\Contracts\FieldValueInterface;
 use Netmex\Lumina\Directives\AbstractDirective;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 
-class CreateDirective extends AbstractDirective implements FieldResolverInterface
+class DeleteDirective extends AbstractDirective implements FieldResolverInterface
 {
-    private SerializerInterface $serializer;
+    private NormalizerInterface $normalizer;
 
-    public function __construct(SerializerInterface $serializer)
+    public function __construct(SerializerInterface $normalizer)
     {
-        $this->serializer = $serializer;
+        $this->normalizer = $normalizer;
     }
 
     public static function name(): string
     {
-        return 'create';
+        return 'delete';
     }
 
     public static function definition(): string
     {
         return <<<'GRAPHQL'
-            directive @create repeatable on ARGUMENT_DEFINITION | INPUT_FIELD_DEFINITION | FIELD_DEFINITION
+            directive @delete(
+                field: String = id
+            ) repeatable on ARGUMENT_DEFINITION | INPUT_FIELD_DEFINITION | FIELD_DEFINITION
         GRAPHQL;
     }
 
     public function resolveField(FieldValueInterface $value, ?QueryBuilder $queryBuilder): callable
     {
         $model = "App\\Entity\\" . $this->modelClass();
-        $serializer = $this->serializer;
+        $normalizer = $this->normalizer;
 
-        return static function (mixed $root, array $arguments, Context $context, ResolveInfo $info) use ($queryBuilder, $model, $serializer)
+        return static function (mixed $root, array $arguments, Context $context, ResolveInfo $info) use ($queryBuilder, $model, $normalizer)
         {
             $entityManager = $queryBuilder->getEntityManager();
+            $entity = $entityManager->find($model, $arguments['id']);
+            $deletedEntity = clone $entity;
 
-            $entity = $serializer->denormalize($arguments, $model);
-
-            $entityManager->persist($entity);
+            $entityManager->remove($entity);
             $entityManager->flush();
 
-            return $serializer->normalize($entity);
+            return $normalizer->normalize($deletedEntity);
         };
     }
 }
