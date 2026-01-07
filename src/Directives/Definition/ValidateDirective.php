0df5075bc04743bea18351f983e31e12012b7b95
Danny <d.verhoeven@vosensetz.nl>
2026-01-07 18:49:32 +0100

    Add ValidateDirective for handling validation operations in GraphQL queries

---
+++ src/Directives/Definition/ValidateDirective.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netmex\Lumina\Directives\Definition;
+
+use Doctrine\ORM\QueryBuilder;
+use Netmex\Lumina\Contracts\ArgumentBuilderDirectiveInterface;
+use Netmex\Lumina\Directives\AbstractDirective;
+
+final class ValidateDirective extends AbstractDirective implements ArgumentBuilderDirectiveInterface
+{
+    public static function name(): string
+    {
+        return 'validate';
+    }
+
+    public static function definition(): string
+    {
+        return <<<'GRAPHQL'
+            directive @validate(
+                field: [String!]
+            ) repeatable on ARGUMENT_DEFINITION | INPUT_FIELD_DEFINITION | FIELD_DEFINITION
+        GRAPHQL;
+    }
+
+    public function handleArgumentBuilder(QueryBuilder $queryBuilder, $value): QueryBuilder
+    {
+        $column = $this->nodeName();
+        $param = ':' . $column."_param";
+
+        $queryBuilder->andWhere("e.$column = $param")
+            ->setParameter($param, $value);
+
+        return $queryBuilder;
+    }
+}
