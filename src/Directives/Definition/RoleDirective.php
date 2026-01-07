65fdae01c4a084d16e14311a4c6bf5b082e37e15
Danny <d.verhoeven@vosensetz.nl>
2026-01-07 19:43:30 +0100

    Add RoleDirective for handling role-based permissions in GraphQL queries

---
+++ src/Directives/Definition/RoleDirective.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netmex\Lumina\Directives\Definition;
+
+use Doctrine\ORM\QueryBuilder;
+use Netmex\Lumina\Contracts\ArgumentBuilderDirectiveInterface;
+use Netmex\Lumina\Directives\AbstractDirective;
+
+final class RoleDirective extends AbstractDirective implements ArgumentBuilderDirectiveInterface
+{
+    public static function name(): string
+    {
+        return 'role';
+    }
+
+    public static function definition(): string
+    {
+        return <<<'GRAPHQL'
+            directive @role(
+                permission: String
+            ) repeatable on ARGUMENT_DEFINITION | INPUT_FIELD_DEFINITION | FIELD_DEFINITION | OBJECT
+        GRAPHQL;
+    }
+
+    public function handleArgumentBuilder(QueryBuilder $queryBuilder, $value): QueryBuilder
+    {
+        throw new \RuntimeException("The @role directive is not yet implemented.");
+
+        return $queryBuilder;
+    }
+}
