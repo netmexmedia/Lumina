40c9801b3f6ce4e077746fa4c8e6b791e0c5871a
Danny <d.verhoeven@vosensetz.nl>
2026-01-07 12:27:28 +0100

    Add OrWhereDirective for enhanced argument handling in GraphQL queries

---
+++ src/Directives/Definition/OrWhereDirective.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netmex\Lumina\Directives\Definition;
+
+use Doctrine\ORM\QueryBuilder;
+use Netmex\Lumina\Contracts\ArgumentBuilderDirectiveInterface;
+use Netmex\Lumina\Directives\AbstractDirective;
+
+final class OrWhereDirective extends AbstractDirective implements ArgumentBuilderDirectiveInterface
+{
+    public static function name(): string
+    {
+        return 'orWhere';
+    }
+
+    public static function definition(): string
+    {
+        return <<<'GRAPHQL'
+            directive @orWhere(
+                on: String
+            ) repeatable on ARGUMENT_DEFINITION | INPUT_FIELD_DEFINITION | FIELD_DEFINITION
+        GRAPHQL;
+    }
+
+    public function handleArgumentBuilder(QueryBuilder $queryBuilder, $value): QueryBuilder
+    {
+        $column = $this->nodeName();
+        $param = ':' . $column."_param";
+
+        $queryBuilder->orWhere("e.$column = $param")
+            ->setParameter($param, $value);
+
+        return $queryBuilder;
+    }
+}
