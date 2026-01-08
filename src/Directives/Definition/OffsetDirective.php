7297311c49746c05d00405847d6eeb6dda9b722b
Danny <d.verhoeven@vosensetz.nl>
2026-01-08 21:04:35 +0100

    Implement FieldArgumentDirectiveInterface in OffsetDirective and add argumentNodes method for offset handling

--- src/Directives/Definition/OffsetDirective.php
+++ src/Directives/Definition/OffsetDirective.php
@@ -3,10 +3,16 @@
 namespace Netmex\Lumina\Directives\Definition;
 
 use Doctrine\ORM\QueryBuilder;
+use GraphQL\Language\AST\InputValueDefinitionNode;
+use GraphQL\Language\AST\NamedTypeNode;
+use GraphQL\Language\AST\NameNode;
+use GraphQL\Language\AST\NodeList;
+use GraphQL\Language\AST\NonNullTypeNode;
 use Netmex\Lumina\Contracts\ArgumentBuilderDirectiveInterface;
+use Netmex\Lumina\Contracts\FieldArgumentDirectiveInterface;
 use Netmex\Lumina\Directives\AbstractDirective;
 
-class OffsetDirective extends AbstractDirective implements ArgumentBuilderDirectiveInterface
+class OffsetDirective extends AbstractDirective implements ArgumentBuilderDirectiveInterface, FieldArgumentDirectiveInterface
 {
     public static function name(): string
     {
@@ -20,6 +26,23 @@ class OffsetDirective extends AbstractDirective implements ArgumentBuilderDirect
                 value: Int,
             ) repeatable on ARGUMENT_DEFINITION | INPUT_FIELD_DEFINITION | FIELD_DEFINITION
         GRAPHQL;
+    }
+
+    public function argumentNodes(): array
+    {
+        return [
+            new InputValueDefinitionNode([
+                'name' => new NameNode(['value' => 'offset']),
+                'type' => new NonNullTypeNode([
+                    'type' => new NamedTypeNode([
+                        'name' => new NameNode(['value' => 'Int']),
+                    ]),
+                ]),
+                'directives' => new NodeList([]),
+                'description' => null,
+                'defaultValue' => null,
+            ])
+        ];
     }
 
     public function handleArgumentBuilder(QueryBuilder $queryBuilder, $value): QueryBuilder
