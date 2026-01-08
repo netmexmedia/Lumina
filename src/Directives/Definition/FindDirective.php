9b43076e13c06bdcdd310b88d19e8eb3b1df5d36
Danny <d.verhoeven@vosensetz.nl>
2026-01-08 19:47:26 +0100

    Implement argument handling in FindDirective by adding argumentNodes method and enhancing resolveField logic

--- src/Directives/Definition/FindDirective.php
+++ src/Directives/Definition/FindDirective.php
@@ -5,14 +5,20 @@ declare(strict_types=1);
 namespace Netmex\Lumina\Directives\Definition;
 
 use Doctrine\ORM\QueryBuilder;
+use GraphQL\Language\AST\InputValueDefinitionNode;
+use GraphQL\Language\AST\NamedTypeNode;
+use GraphQL\Language\AST\NameNode;
+use GraphQL\Language\AST\NodeList;
 use GraphQL\Type\Definition\ResolveInfo;
 use Netmex\Lumina\Context\Context;
+use Netmex\Lumina\Contracts\FieldArgumentDirectiveInterface;
+use Netmex\Lumina\Contracts\FieldInputDirectiveInterface;
 use Netmex\Lumina\Contracts\FieldResolverInterface;
 use Netmex\Lumina\Contracts\FieldValueInterface;
 use Netmex\Lumina\Directives\AbstractDirective;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
-final class FindDirective extends AbstractDirective implements FieldResolverInterface
+final class FindDirective extends AbstractDirective implements FieldResolverInterface, FieldArgumentDirectiveInterface
 {
     private NormalizerInterface $normalizer;
 
@@ -36,13 +42,35 @@ final class FindDirective extends AbstractDirective implements FieldResolverInte
         GRAPHQL;
     }
 
+    public function argumentNodes(): array
+    {
+        return [
+            new InputValueDefinitionNode([
+                'name' => new NameNode(['value' => 'id']),
+                'type' => new NamedTypeNode([
+                    'name' => new NameNode(['value' => 'Int'])
+                ]),
+                'directives' => new NodeList([]),
+                'description' => null,
+                'defaultValue' => null,
+            ])
+        ];
+    }
+
     public function resolveField(FieldValueInterface $value, ?QueryBuilder $queryBuilder): callable
     {
         $normalizer = $this->normalizer;
 
         return static function (mixed $root, array $args, Context $context, ResolveInfo $info) use ($queryBuilder, $normalizer)
         {
-            $result = $queryBuilder->getQuery()->getSingleResult();
+            $alias = current($queryBuilder->getRootAliases());
+
+            $id = $args['id'] ?? null;
+            $result = $queryBuilder
+                ->where("$alias.id = :id")
+                ->setParameter('id', $id)
+                ->getQuery()
+                ->getOneOrNullResult();
 
             return $normalizer->normalize($result, null, [
                 'circular_reference_handler' => function ($object) {
