127b98adf1cbbb0842cab63fde6a17646062fd45
Danny <d.verhoeven@vosensetz.nl>
2026-01-10 00:51:51 +0100

    Refactor CountDirective to use column argument instead of field; add new SumDirective for summation functionality

--- src/Directives/Definition/SumDirective.php
+++ src/Directives/Definition/SumDirective.php
@@ -5,36 +5,27 @@ namespace Netmex\Lumina\Directives\Definition;
 use Doctrine\ORM\QueryBuilder;
 use GraphQL\Language\AST\DocumentNode;
 use GraphQL\Language\AST\FieldDefinitionNode;
-use GraphQL\Language\AST\InputValueDefinitionNode;
 use GraphQL\Language\AST\NamedTypeNode;
 use GraphQL\Language\AST\NameNode;
-use GraphQL\Language\AST\NodeList;
 use GraphQL\Language\AST\NonNullTypeNode;
-use GraphQL\Language\AST\ObjectTypeDefinitionNode;
 use GraphQL\Type\Definition\ResolveInfo;
 use Netmex\Lumina\Context\Context;
-use Netmex\Lumina\Contracts\ArgumentBuilderDirectiveInterface;
-use Netmex\Lumina\Contracts\FieldArgumentDirectiveInterface;
-use Netmex\Lumina\Contracts\FieldInputDirectiveInterface;
 use Netmex\Lumina\Contracts\FieldResolverInterface;
 use Netmex\Lumina\Contracts\FieldTypeModifierInterface;
 use Netmex\Lumina\Contracts\FieldValueInterface;
 use Netmex\Lumina\Directives\AbstractDirective;
-use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
-use Symfony\Component\Serializer\SerializerInterface;
 
-class CountDirective extends AbstractDirective implements FieldResolverInterface, FieldTypeModifierInterface
+class SumDirective extends AbstractDirective implements FieldResolverInterface, FieldTypeModifierInterface
 {
-
     public static function name(): string
     {
-        return 'count';
+        return 'sum';
     }
 
     public static function definition(): string
     {
         return <<<'GRAPHQL'
-            directive @count(
+            directive @sum(
                 column: String = id
             ) repeatable on ARGUMENT_DEFINITION | INPUT_FIELD_DEFINITION | FIELD_DEFINITION
         GRAPHQL;
@@ -47,14 +38,16 @@ class CountDirective extends AbstractDirective implements FieldResolverInterface
         return static function (mixed $root, array $arguments, Context $context, ResolveInfo $info) use ($queryBuilder, $sumField)
         {
             $alias = current($queryBuilder->getRootAliases());
+
             $queryBuilder->resetDQLPart('select');
 
-            $result =  $queryBuilder
-                ->select("COUNT($alias.$sumField) AS count")
+
+            $result = $queryBuilder
+                ->select("SUM($alias.$sumField) AS sumResult")
                 ->getQuery()
                 ->getSingleScalarResult();
 
-            return $result;
+            return (int) $result;
         };
     }
 
